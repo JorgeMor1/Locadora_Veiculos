e0e5e811f8c150fe2d439301615010c23d77be03
Jorge Luiz <batistajoorge@gmail.com>
2025-03-06 20:24:05 -0300

    Adicionado a criação de veículo e adcionada as funcionalidades na classe Main

--- Gerenciadores_E_Entidades/GerenciadorVeiculos.java
+++ Gerenciadores_E_Entidades/GerenciadorVeiculos.java
@@ -1,5 +1,7 @@
 package Gerenciadores_E_Entidades;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class GerenciadorVeiculos extends Veiculos {
@@ -20,6 +22,19 @@ public class GerenciadorVeiculos extends Veiculos {
 
     public void cadastraVeiculo() {
         Scanner sc = new Scanner(System.in);
+        List<Object> veiculos = new ArrayList<>();
+        
+        System.out.print("Digite o nome da marca de seu veículo: ");
+        veiculos.add(sc.nextLine());
+        System.out.print("Digite o nome do modelo do seu veículo: ");
+        veiculos.add(sc.nextLine());
+        System.out.print("Digite a quilometragem atual do veículo: ");
+        veiculos.add(sc.nextInt());
+        System.out.print("Digite o ano do veículo: ");
+        veiculos.add(sc.nextInt());
 
+        System.out.println("Veículo cadastrado com sucesso!");
+
+        sc.close();
     }
 }

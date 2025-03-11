7fc59bcf07cdbc621857acc4b595db842b52fcad
Jorge Luiz <batistajoorge@gmail.com>
2025-03-10 21:11:53 -0300

    feat: Implementa enum para marcas e melhora tratamento de erros no aluguel

--- Gerenciadores_E_Entidades/GerenciadorAlugueis.java
+++ Gerenciadores_E_Entidades/GerenciadorAlugueis.java
@@ -20,13 +20,48 @@ public class GerenciadorAlugueis extends Alugueis {
         Scanner sc = new Scanner(System.in);
         var scheduler = Executors.newScheduledThreadPool(1);
         GerenciadorClientes gerenciadorClientes = new GerenciadorClientes();
+        Boolean semMarca;
+        Marcas carro = Marcas.HYUNDAI;
+        Marcas moto = Marcas.KAWASAKI;
+        do {
+            semMarca = false;
+            String veiculoInteresse;
+            try {
+                System.out.println("Você deseja um Carro ou uma Moto? \n");
+                veiculoInteresse = sc.nextLine().toUpperCase();
+                if (!veiculoInteresse.equals("CARRO") && !veiculoInteresse.equals("MOTO")) {
+                    throw new IllegalArgumentException("Opção inválida! Digite CARRO ou MOTO.");
+                }
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+                semMarca = true;
+                continue;
+            }
+            System.out.println("Qual a quantidade de meses de aluguel ? ");
+            Integer quantMeses = sc.nextInt();
+            sc.nextLine();
+            List<Veiculos> veiculosDisponiveis = new ArrayList<>();
+            if (veiculoInteresse.equals("CARRO")) {
+                veiculosDisponiveis.add(new Veiculos("HB20", 10000, 2019));
+                {
+                    Veiculos veiculo = veiculosDisponiveis.get(0);
+                    System.out.println("Temos o seguintes modelos e a taxa de aluguel: " +
+                            "Marca: " + carro + "\n" +
+                            "Modelo: " + veiculo.getModelo() + "\n" +
+                            "Quilometragem Atual: " + veiculo.getQuilometragem() + "\n" +
+                            "Taxa de Aluguel: " + veiculo.taxa(carro.getValor(), quantMeses) + "\n");
+                }
+            } else if (veiculoInteresse.equals("MOTO")) {
+                veiculosDisponiveis.add(new Veiculos("NINJA300", 9000, 2022));
+                Veiculos veiculo = veiculosDisponiveis.get(0);
+                System.out.println("Temos o seguintes modelos e a taxa de aluguel: " +
+                        "Marca: " + moto + "\n" +
+                        "Modelo: " + veiculo.getModelo() + "\n" +
+                        "Quilometragem Atual: " + veiculo.getQuilometragem() + "\n" +
+                        "Taxa de Aluguel: " + veiculo.taxa(carro.getValor(), quantMeses) + "\n");
+            }
+        } while (semMarca);
 
-        List<Object> aluguel = new ArrayList<>();
-        aluguel.add("HYUNDAI");
-        aluguel.add("HB20");
-        aluguel.add(2015);
-        aluguel.add(10000);
-        System.out.println("Temos esse(s) veículo(s) disponível: \n" + aluguel + "\n");
         String resposta;
         Boolean x;
         do {
@@ -50,14 +85,13 @@ public class GerenciadorAlugueis extends Alugueis {
             }
             System.out.println("Reservando seu veículo. Por favor, aguarde...");
             scheduler.schedule(() -> {
-            System.out.println("Seu veículo foi reservado com sucesso!!! \n"
-                    + "Venha até nossa concessionária para buscar seu veículo!");
+                System.out.println("Seu veículo foi reservado com sucesso!!! \n"
+                        + "Venha até nossa concessionária para buscar seu veículo!");
             }, 4, TimeUnit.SECONDS);
             scheduler.shutdown();
         } else if (resposta.equals("N")) {
-            System.out.println("Na próxima teremos mais veículos!");
+            System.out.println("Lamentamos! Na próxima teremos mais veículos!");
         }
-        sc.close();
     }
 
 }
